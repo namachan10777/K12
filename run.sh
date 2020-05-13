7fafb8dfdeddfbcefabf0ff2d7141b35a56770d8
Nakano Masaki <namachan10777@gmail.com>
2020-05-13 16:14:53 +0900

    gdb

--- run.sh
+++ run.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-qemu-system-x86_64 --enable-kvm -m 1024 -boot order=d -bios ./OVMF.fd ./K12 -net user,hostfwd=tcp::10022-:22 -net nic
+GDBPORT=10000
+SSHPORT=10022
+
+echo "gdb port:"$GDBPORT
+echo "ssh port:"$SSHPORT
+
+qemu-system-x86_64 \
+	--enable-kvm \
+	-m 1024 \
+	-boot order=d \
+	-bios ./OVMF.fd \
+	-gdb tcp::$GDBPORT \
+	-net user,hostfwd=tcp::$SSHPORT-:22 -net nic \
+	./K12
